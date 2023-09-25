62fffd4f2fbd478d69ddedd424370cb35c133ba9
Jonas Lindemann <jonas.lindemann@lunarc.lu.se>
2023-09-25 23:26:07 +0200

    Removed not needed build files.

--- config_debug_build.cmd
+++
@@ -1,9 +0,0 @@
-if exist build-debug (
-    rmdir /Q /S build-debug
-)
-
-mkdir build-debug
-
-pushd build-debug
-cmake -DCMAKE_BUILD_TYPE=Debug ..
-popd
--- config_release_build.cmd
+++
@@ -1,9 +0,0 @@
-if exist build-release (
-    rmdir /Q /S build-release
-)
-
-mkdir build-release
-
-pushd build-release
-cmake -DCMAKE_BUILD_TYPE=Release ..
-popd
